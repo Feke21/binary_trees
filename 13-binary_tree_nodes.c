e273276854bdccf3154a5f1f7c7237d6defa73ed
Feke_21 <favouredubio@gmail.com>
2023-06-06 15:11:43 +0000

    Fixing binary tree nodes file accord. to betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,7 +9,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	
+
 	return (((tree->left || tree->right) ? 1 : 0) +
 			binary_tree_nodes(tree->left) +
 			binary_tree_nodes(tree->right));
