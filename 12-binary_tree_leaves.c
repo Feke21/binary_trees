ed1a530d9598bb9aa8a21b13317b8c0851e33530
Feke_21 <favouredubio@gmail.com>
2023-06-06 15:00:13 +0000

    Fixing binary tree leaves count file

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,10 +10,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left && tree->right == NULL)
-	{
-		return (1);
-	}
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (((tree->left && tree->right == NULL) ? 1 : 0) +
+			binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 }
