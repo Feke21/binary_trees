7190b2caf7dd75fc4258a6bb8752a3602070264e
Feke_21 <favouredubio@gmail.com>
2023-06-06 15:21:48 +0000

    Adding binary tree balance file and updating header file

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height
+ * Return: If tree is NULL 0, otherwise height of the node
+**/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (!tree)
+		return (0);
+
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+
+	return ((l > r ? l : r) + 1);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: 0 if tree is NULL
+**/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
